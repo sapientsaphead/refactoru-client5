f2c002a7b7762f8c9be27a51f28fc4143724a4d1
Debra Zapata <debra.zapata@gmail.com>
2013-09-26 16:02:43 -0600

    Thursday changes.

--- quotes.js
+++ quotes.js
@@ -11,50 +11,81 @@ $(".submitnewquote").on("click", function(){
 // Function to output quote submission into main page.
 
 var newquote = {};
-var newquotelist =[];
-var author;
-var quote;
+var newquotelist = [];
+var id = 0;
 
-	
-	newquote.num = 0;
-	newquote.author;
-	newquote.quote;
+var getQuote = function(author, quote, timeStamp) {
+	newquotelist[id] = {
+		num: id,
+		author: author,
+		quote: quote,
+		timeStamp: timeStamp,
+	}
+	id++;
+};
+
+console.log("var id outside", id);
 
 
 $(".submitquote").on("click", function(event) {
-	event.preventDefault();
-
-
-	$(".newauthor").trigger("focusout");
-	$(".newquote").trigger("focusout");
+	//event.preventDefault ? event.preventDefault() : event.returnValue = false;
 	
 	var author = $(".newauthor").val();
 	var quote = $(".newquote").val();
-		newquote.author = author;
-		newquote.quotetext = quote;
-		newquote.num +=1;	
+	var timeStamp = new Date();
 
-	$(".quotebox").prepend("<div class=\"author\"><p>" + author + "</p></div>" 
-		+ "<div class=\"quote\"><p>\"" + quote + "\"</p></div>" 
-		+ "<div class=\"rating\"><span class=\"star\">☆</span><span class=\"star\">☆</span><span class=\"star\">☆</span><span class=\"star\">☆</span><span class=\"star\">☆</span>	`</div>");
+	console.log("before if newquotelist", newquotelist);
+	console.log("id before if", id);
 
-	console.log("iauthor", author);
-	console.log("iquote", quote);
-	console.log("inewquote", newquote);
+		if(author && quote) {
+			getQuote(author, quote, timeStamp);
+			console.log("after if newquotelist", newquotelist);
+			console.log("id after if", id);
+			console.log("var id", id-1);
 
-	newquotelist.push(newquote);
-	console.log("inewquotelist", newquotelist);
+			$(".quotelist").prepend("<div class=\"quotebox\" id=\"" + newquotelist[id-1].num + "\"><div class=\"author\"><p>" + author + "</p></div>" 
+			+ "<div class=\"quote\"><p>\"" + quote + "\"</p></div>" 
+			+ "<div class=\"rating\"><span class=\"star\" id=\"5s\">☆</span><span class=\"star\" id=\"4s\">☆</span><span class=\"star\" id=\"3s\">☆</span><span class=\"star\" id=\"2s\">☆</span><span class=\"star\" id=\"1s\">☆</span></div></div>");
+		
+		// Close submit new quote box & empty.
+		$(".newlightbox").css("display","none");
+		$(".newauthor").val("");
+		$(".newquote").val("");
 
-	// Close submit new quote box & empty.
+		}
+		// Alert plus keep box open.
+		else {
+			alert("Please fill out the form completely!")
+		}
+		newquotelist.push(newquote);
+
+		// Add star rating for each quote to object.
+
+		var starrating;
+		var idnum;
+
+
+		$(".star").on("click", function(){
+			starrating = $(this).attr("id");
+			idnum = $(this).parent().parent().attr("id");
+
+			newquotelist[idnum]["rating"] = starrating;
+
+			$(this).nextAll().andSelf().css("color","gold");
+			$(this).prevAll().css("color", "black");
+			});
+
+			
+		});
+
+
+
+
+// Cancel submit and close new quote lightbox.
+		
+$(".cancel").on("click", function() {
 	$(".newlightbox").css("display","none");
-	
-
-
 });
-
-console.log("onewquote", newquote)
-console.log("onewquote", newquote)
-console.log("onewquotelist", newquotelist)
 
 
 // Display random quote lightbox.
